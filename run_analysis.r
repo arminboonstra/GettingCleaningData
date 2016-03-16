30aa34ca4fda7edf67bb03d0f6c0e45b1e4df24a
arminboonstra <armin.boonstra@yahoo.com>
2016-03-16 08:15:03 +0100

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -5,7 +5,8 @@
 #  4. Appropriately labels the data set with descriptive variable names. 
 #  
 #  5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-  
+#  I first completed this assignment in september. I'm retaking this course as part of the complete Data Science specialisation.
+
 library (dplyr)
 library (plyr)
 library(reshape2)
